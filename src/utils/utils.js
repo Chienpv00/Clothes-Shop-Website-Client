3fa9b97b38fdf3a1937e92b6c9bdf75a7f38e153
chienpv <chienpv00@gmail.com>
2022-06-17 15:34:21 +0700

    add: new utils useCategories

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,11 +1,13 @@
 import { gql, useQuery } from '@apollo/client'
 import { useState } from 'react'
 import { GET_USER } from '~/config/queryGQLConst/user'
+import { GET_CAT_CONSTANT } from '~/config/queryGQLConst/productConstant'
 
 export function useUser() {
     const [user, setUser] = useState()
 
     const { loading, error } = useQuery(GET_USER, {
+        fetchPolicy: 'no-cache',
         onCompleted: (data) => {
             setUser(data.getUser)
         },
@@ -18,3 +20,17 @@ export function useUser() {
         error,
     }
 }
+
+export function useCategories() {
+    const [categories, setCategories] = useState()
+    const { loading, error } = useQuery(GET_CAT_CONSTANT, {
+        
+        onCompleted: (data) => {
+            fetchPolicy: 'no-cache',
+            setCategories(data.getCatsSchema)
+        },
+    })
+    return { categories, setCategories, loading, error }
+}
+
+
