c49bc656bb2a4215afc7aa561561870794ac62e9
chienpv <chienpv00@gmail.com>
2022-06-17 15:36:26 +0700

    Fix: Connect api -> product product page

--- src/pages/Product/index.js
+++ src/pages/Product/index.js
@@ -4,39 +4,38 @@ import { Helmet } from 'react-helmet-async'
 
 import styles from './Product.module.scss'
 import Title from '~/components/Title'
-import { somis } from '~/data'
 import { Wrapper as ProductWrapper, FlexWrapper } from '~/components/Popper'
 import Button from '~/components/Button'
-import { useQuery } from '~/hooks'
+import { useQueryHook } from '~/hooks'
 import ProductItem from '~/components/ProductItem'
-
+import { useQuery } from '@apollo/client'
+import constant from '~/config/constant'
+import api from '~/config/api'
 const cx = classNames.bind(styles)
 
-const categories = [
-    { id: 'ao-so-mi', name: 'Áo Sơ Mi' },
-    { id: 'ao-kieu', name: 'Áo Kiểu' },
-    { id: 'ao-thun', name: 'Áo Thun' },
-    { id: 'quan-jean', name: 'Quần Jean' },
-    { id: 'quan-kieu', name: 'Quần Kiểu' },
-    { id: 'chan-vay', name: 'Chân Váy' },
-    { id: 'dam', name: 'Đầm' },
-]
 function Product() {
-    let query = useQuery().get('type')
+    let query = useQueryHook().get('type')
 
-    const title = categories.filter((category) => category.id === query)
+    const [title] = constant.product.categories.filter((category) => category.id === query)
+    const { data, loading } = useQuery(api.queries.product.PRODUCTS_FOR_PRODUCT, {
+        variables: { category: title.id, limit: constant.product.LIMIT_PRODUCT_PAGE },
+    })
+
+    if (loading) {
+        return <div>Đang tải ...</div>
+    }
 
     return (
         <Fragment>
             <Helmet>
-                <title>{title[0].name}</title>
+                <title>{title.name}</title>
             </Helmet>
             <div className={cx('wrapper')}>
-                <Title content={title[0].name} />
+                <Title content={title.name} />
                 <ProductWrapper flexWrapper>
-                    {somis.map((somi) => (
-                        <FlexWrapper key={somi.id}>
-                            <ProductItem product={somi} />
+                    {data.getProductByCat.map((item) => (
+                        <FlexWrapper key={item.id}>
+                            <ProductItem product={item} />
                         </FlexWrapper>
                     ))}
                 </ProductWrapper>
